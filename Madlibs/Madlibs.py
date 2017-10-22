5ce259c577e5b5640b486f055d2b2f32a8ec5838
Zer0-M <msj2340@gmail.com>
2017-10-21 23:42:12 -0400

    Madlibs.py supports user input

--- Madlibs/Madlibs.py
+++ Madlibs/Madlibs.py
@@ -5,7 +5,7 @@ Places=['Gotham City', 'Metropolis', 'Atlantis', 'Springfield', 'Hollywood', 'Ca
 Noun=['factory reset button', 'idiot', 'toaster', 'legend', 'death wish', 'therapy', 'french chef', 'antidepressant drug', 'candlestick maker', 'brethren', 'national security agency', 'useless brakes', 'international law enforcement agency', 'sound barrier', 'mad cow disease']
 Verb=['surround', 'stab', 'return', 'medicate', 'blindside', 'boogie', 'flap', 'trip', 'swat', 'suck in', 'harasses', 'traps', 'snoops', 'explode', 'scatter', 'challenge', 'fight', 'bury', 'splatter', 'smacks']
 Adjective=['dead', 'hairless', 'sadistic', 'metal', 'wild', 'domesticated', 'abnormal', 'medicated', 'cocky', 'vengeful', 'sinister', 'costumed', 'cowardly', 'haunting', 'startled', 'alcoholic', 'demanding', 'shivering', 'offensive']
-
+print 'Welcome to Madlibs\nMade by Mohammed Jamil\n'
 def extractText(file):
     source=open(file,'r')
     content=source.read()
@@ -16,7 +16,7 @@ def extractText(file):
     content=content.replace('?',' ?')
     content=content.replace('!',' !')
     return content
-print extractText('story0.txt')
+
 
 def extractBlanks(story):
     spacePos=story.find(' ')
@@ -29,25 +29,38 @@ def extractBlanks(story):
         spacePos=story.find(' ')
     return gatherer
 
-print extractBlanks(extractText('story0.txt'))
 
 def gatherWords(toBeReplaced):
     filledin=[]
-    for blank in toBeReplaced:
-        if blank == 'noun*':
-            filledin+=[random.choice(Noun)]
-        elif blank == 'verb*':
-            filledin+=[random.choice(Verb)]
-        elif blank == 'adjective*':
-            filledin+=[random.choice(Adjective)]
-        elif blank == 'name*':
-            filledin+=[random.choice(Names)]
-        elif blank == 'place*':
-            filledin+=[random.choice(Places)]
+    if raw_input('''do you want to input the words yourself?[Y/N]
+if not the Madlib will be filled using a built in word database: ''')=='Y':
+        for blank in toBeReplaced:
+       
+            if blank == 'noun*':
+                filledin+=[raw_input('noun ')]
+            elif blank == 'verb*':
+                filledin+=[raw_input('verb ')]
+            elif blank == 'adjective*':
+                filledin+=[raw_input('adjective ')]
+            elif blank == 'name*':
+                filledin+=[raw_input('name ')]
+            elif blank == 'place*':
+                filledin+=[raw_input('place ')]
+    else:
+        for blank in toBeReplaced:
+            if blank == 'noun*':
+                filledin+=[random.choice(Noun)]
+            elif blank == 'verb*':
+                filledin+=[random.choice(Verb)]
+            elif blank == 'adjective*':
+                filledin+=[random.choice(Adjective)]
+            elif blank == 'name*':
+                filledin+=[random.choice(Names)]
+            elif blank == 'place*':
+                filledin+=[random.choice(Places)]
+            
     return filledin
             
-print gatherWords(extractBlanks(extractText('story0.txt')))
-print gatherWords(extractBlanks(extractText('story1.txt')))
 
 def fillBlanks(story,fillers):
     spacePos=story.find(' ')
@@ -64,7 +77,7 @@ def fillBlanks(story,fillers):
         spacePos=story.find(' ')
     return filledinstory
     
-#print fillBlanks(extractText('story0.txt'), tofill)
+
 
 def outputstory(file, filledin):
     file=file.replace('.txt','')
@@ -80,6 +93,32 @@ def MadLibs(file):
     filledinstory=fillBlanks(extractText(file), tofill)
     outputstory(file,filledinstory)
     return filledinstory
+def personalStory():
+    filesToBeFilled=[]
+    gatherer=''
+    file='  '
+    while file != '':
+        file=raw_input(''' Do you want to use your own story? Type the filename(it has to be a text file) with extension. If you do not want to input press enter: ''')
+        if file!= '':
+            filesToBeFilled += [file]
+    for files in filesToBeFilled:
+        gatherer+=MadLibs(files)+'\n'
+    return gatherer
     
+print 'Sample Story\n'
 print MadLibs('story0.txt')
-print MadLibs('story1.txt')
+print 'Another Sample Story\n'
+print MadLibs('story1.txt') 
+print '\n'       
+print personalStory()
+print '\n'
+def end():
+    
+    if raw_input('Do you want to do more Madlibs?[Y/N]: ')== 'Y':
+        return raw_input('Then restart the program again: ')+'\n'
+    else:
+        return raw_input('Bye')
+print end()
+        
+    
+
